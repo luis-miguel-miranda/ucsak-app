781102a01d14ee6de33c4118ca53762fca085019
Lars George <lars.george@databricks.com>
2025-03-13 20:21:13 +0100

    Added 404 handler

--- api/app.py
+++ api/app.py
@@ -10,16 +10,19 @@ load_dotenv()
 assert os.getenv(
     'DATABRICKS_WAREHOUSE_ID'), "DATABRICKS_WAREHOUSE_ID must be set in app.yaml."
 
-app = Flask(__name__, static_folder='../build', static_url_path='/')
+app = Flask(__name__, 
+    static_folder='../build',  # Path to your React build folder
+    static_url_path=''         # Empty string to serve from root
+)
 CORS(app)
 
 # Register routes from each module
-catalog_commander_routes.register_routes(app)
-data_contract_routes.register_routes(app)
 data_product_routes.register_routes(app)
-settings_routes.register_routes(app)
+data_contract_routes.register_routes(app)
 business_glossary_routes.register_routes(app)
 entitlements_routes.register_routes(app)
+catalog_commander_routes.register_routes(app)
+settings_routes.register_routes(app)
 notifications_routes.register_routes(app)
 search_routes.register_routes(app)
 
@@ -32,6 +35,17 @@ def get_current_time():
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def index(path):
+    """
+    This is the catch-all route for the root URL. It will return the index.html file from the build folder.
+    """
+    return app.send_static_file('index.html')
+
+
+@app.errorhandler(404)
+def not_found(e):
+    """
+    This is a catch-all route for 404 errors. It will return the index.html file from the build folder.
+    """
     return app.send_static_file('index.html')
 
 
